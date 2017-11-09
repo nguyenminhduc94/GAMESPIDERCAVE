f43f64e0474ed5a0e6dc2d54c64fc62af06a12bd
nguyenminhduc94 <>
2017-11-09 10:55:35 +0700

    add .

---
+++ Assets/Scripts/Door Script/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	private Animator anim;
+	private bool isTouch;
+	// Use this for initialization
+	void Awake(){
+		anim = GetComponent<Animator> ();
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		
+	}
+}
