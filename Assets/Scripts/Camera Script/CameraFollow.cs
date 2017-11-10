a3b24a64de58e2d4db6c88512d565e49ae876808
nguyenminhduc94 <>
2017-11-10 11:56:57 +0700

    add .

--- Assets/Scripts/Camera Script/CameraFollow.cs
+++ Assets/Scripts/Camera Script/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	private Transform player;
-
+	public float minX, maxX;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("CharacterMoving").transform;
@@ -16,6 +16,15 @@ public class CameraFollow : MonoBehaviour {
 		if(player != null){
 			Vector3 vc3 = transform.position;
 			vc3.x = player.position.x;
+
+
+			if(vc3.x < minX){
+				vc3.x = minX;
+			}
+
+			if(vc3.x > maxX){
+				vc3.x = maxX;
+			}
 			transform.position = vc3;
 		}
 	}
